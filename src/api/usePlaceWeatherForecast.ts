4dcb2270d0adfd31cefb5c5de617340748d463fc
xnauax <62560321+xnauax@users.noreply.github.com>
2022-02-08 01:18:33 -0300

    chore: changes querys

--- src/api/usePlaceWeatherForecast.ts
+++ src/api/usePlaceWeatherForecast.ts
@@ -3,11 +3,11 @@ import { useQuery } from "react-query";
 import { API_TOKEN_WEATHER, API_URL_WEATHER } from "../App.constants";
 import { toast } from "../lib/Toast";
 
-export function usePlaceWeather(location: string, units : 'metric' | 'imperial') {
+export function usePlaceWeatherForecast(location: string, units : 'metric' | 'imperial') {
   return useQuery({
     queryKey: ["weather", location, units],
     queryFn: async () => {
-      const { data } = await axios.get(`${API_URL_WEATHER}/weather`, {
+      const { data } = await axios.get(`${API_URL_WEATHER}/forecast`, {
         params: {
           city: location,
           KEY: API_TOKEN_WEATHER,
@@ -17,7 +17,7 @@ export function usePlaceWeather(location: string, units : 'metric' | 'imperial')
 
       return data;
     },
-    onError: () => toast("Não foi possível carregar o clima da cidade escolhida"),
+    onError: () => toast("Não foi possível carregar os detalhes da cidade escolhida"),
     enabled: Boolean(location.trim())
   });
 }
