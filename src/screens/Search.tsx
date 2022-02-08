e36ee6ccc40859aa1ce87afcc38758ec1fce84cd
xnauax <62560321+xnauax@users.noreply.github.com>
2022-02-08 01:25:32 -0300

    wip: Search screen

---
+++ src/screens/Search.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export function Search() {
+  return (
+    <View style={styles.root}>
+      <Text>Está nos detalhes</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})
