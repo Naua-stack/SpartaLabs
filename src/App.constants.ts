44420b3f999ebd7e512d2bac9ce59a9a9aa198bb
xnauax <62560321+xnauax@users.noreply.github.com>
2022-02-07 22:52:17 -0300

    chore: change names consts

--- src/App.constants.ts
+++ src/App.constants.ts
@@ -1,4 +1,7 @@
-import {ENV_API_TOKEN_WEATHER, ENV_API_TOKEN_GOOGLE_PLACES} from '@env';
+import {ENV_API_TOKEN_WEATHER, ENV_API_TOKEN_GOOGLE} from '@env';
 
 export const API_TOKEN_WEATHER = ENV_API_TOKEN_WEATHER;
-export const API_TOKEN_GOOGLE_PLACES = ENV_API_TOKEN_GOOGLE_PLACES;
+export const API_TOKEN_GOOGLE= ENV_API_TOKEN_GOOGLE;
+
+export const API_URL_GOOGLE = 'https://maps.googleapis.com/maps/api';
+export const API_URL_WEATHER = 'http://api.openweathermap.org/data/2.5/'
