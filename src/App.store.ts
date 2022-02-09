8c24fe8bea5e6dd1d095c4e4f236c6bb91d58044
Nauã Fernandes <nauafernandesvv@gmail.com>
2022-02-09 13:21:03 -0300

    chore: favorites and remove place

--- src/App.store.ts
+++ src/App.store.ts
@@ -6,10 +6,19 @@ export const useStoredPlaces = create(
   persist(
     (set, get: any) => ({
       places: [] as string[],
+      favorites: [] as string[],
 
       addPlace: (payload: string) =>
         set({ places: [...new Set([...get().places, payload])] }),
       removePlaces: () => set({ places: [] }),
+      favoriteToggle: (payload: string) =>
+        set({ favorites: [...new Set([...get().favorites, payload])] }),
+      unfavoriteToggle: (payload: string) =>
+        set({
+          favorites: get().favorites.filter(
+            (place: string) => place !== payload
+          ),
+        }),
       removePlace: (payload: string) =>
         set({
           places: get().places.filter((place: string) => place !== payload),
