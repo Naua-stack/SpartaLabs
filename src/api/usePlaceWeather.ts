4c9c426b6a72dda9e69c18f1a312e6a4e7ef5ec4
xnauax <62560321+xnauax@users.noreply.github.com>
2022-02-09 00:04:46 -0300

    chore: change params city

--- src/api/usePlaceWeather.ts
+++ src/api/usePlaceWeather.ts
@@ -3,21 +3,27 @@ import { useQuery } from "react-query";
 import { API_TOKEN_WEATHER, API_URL_WEATHER } from "../App.constants";
 import { toast } from "../lib/Toast";
 
-export function usePlaceWeather(location: string, units : 'metric' | 'imperial') {
+export function usePlaceWeather(
+  location: string,
+  units: "metric" | "imperial"
+) {
+  console.log(location, units, API_TOKEN_WEATHER, API_URL_WEATHER);
   return useQuery({
     queryKey: ["weather", location, units],
     queryFn: async () => {
-      const { data } = await axios.get(`${API_URL_WEATHER}/weather`, {
+      const { data } = await axios.get(`${API_URL_WEATHER}weather`, {
         params: {
-          city: location,
-          KEY: API_TOKEN_WEATHER,
-          units: units
+          q: location,
+          lang: "pt_br",
+          appid: API_TOKEN_WEATHER,
+          units: units,
         },
       });
 
       return data;
     },
-    onError: () => toast("Não foi possível carregar o clima da cidade escolhida"),
-    enabled: Boolean(location.trim())
+    onError: () =>
+      toast("Não foi possível carregar o clima da cidade escolhida"),
+    enabled: Boolean(location.trim()),
   });
 }
--- src/api/usePlaceWeatherForecast.ts
+++ src/api/usePlaceWeatherForecast.ts
@@ -3,21 +3,25 @@ import { useQuery } from "react-query";
 import { API_TOKEN_WEATHER, API_URL_WEATHER } from "../App.constants";
 import { toast } from "../lib/Toast";
 
-export function usePlaceWeatherForecast(location: string, units : 'metric' | 'imperial') {
+export function usePlaceWeatherForecast(
+  location: string,
+  units: "metric" | "imperial"
+) {
   return useQuery({
     queryKey: ["weather", location, units],
     queryFn: async () => {
       const { data } = await axios.get(`${API_URL_WEATHER}/forecast`, {
         params: {
-          city: location,
-          KEY: API_TOKEN_WEATHER,
-          units: units
+          q: location,
+          appid: API_TOKEN_WEATHER,
+          units: units,
         },
       });
 
       return data;
     },
-    onError: () => toast("Não foi possível carregar os detalhes da cidade escolhida"),
-    enabled: Boolean(location.trim())
+    onError: () =>
+      toast("Não foi possível carregar os detalhes da cidade escolhida"),
+    enabled: Boolean(location.trim()),
   });
 }
