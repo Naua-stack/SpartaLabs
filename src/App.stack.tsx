d465c0ebaf5a76d3a0b5b9ada2ff375b701099bb
xnauax <62560321+xnauax@users.noreply.github.com>
2022-02-09 00:03:00 -0300

    chore: config header routes

--- src/App.stack.tsx
+++ src/App.stack.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Start, Details, Search } from "./screens";
-
+import { Citys, Details, Search } from "./screens";
 const { Navigator, Screen } = createStackNavigator();
 
 export function Stack() {
   return (
     <NavigationContainer>
-      <Navigator initialRouteName="Start">
-        <Screen name="Start" component={Start} />
+      <Navigator
+        initialRouteName="Citys"
+        headerMode="none"
+        screenOptions={{
+          cardStyle: {
+            backgroundColor: "transparent",
+          },
+        }}
+      >
+        <Screen name="Citys" component={Citys} />
         <Screen name="Details" component={Details} />
         <Screen name="Search" component={Search} />
       </Navigator>
