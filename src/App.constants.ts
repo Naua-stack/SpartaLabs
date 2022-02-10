cbc7b82e9ae7f15b9cff9fa14772e4388420daf1
xnauax <62560321+xnauax@users.noreply.github.com>
2022-02-10 01:43:02 -0300

    chore: constant status bar height

--- src/App.constants.ts
+++ src/App.constants.ts
@@ -1,7 +1,8 @@
 import { ENV_API_TOKEN_WEATHER, ENV_API_TOKEN_GOOGLE } from "@env";
-
+import Constants from "expo-constants";
 export const API_TOKEN_WEATHER = ENV_API_TOKEN_WEATHER;
 export const API_TOKEN_GOOGLE = ENV_API_TOKEN_GOOGLE;
 
 export const API_URL_GOOGLE = "https://maps.googleapis.com/maps/api";
 export const API_URL_WEATHER = "https://api.openweathermap.org/data/2.5/";
+export const statusBarHeight = Constants.statusBarHeight;
