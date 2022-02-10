73e63f53fddadae5d5cbd0f8515b58ec32294827
Nauã Fernandes <nauafernandesvv@gmail.com>
2022-02-10 13:29:07 -0300

    chore: change type metric

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -8,67 +8,63 @@ import {
 import { usePlaceWeatherForecast } from "../api";
 import { CardDetailsDays } from "../components/CardDetailsDays";
 import { Ionicons } from "@expo/vector-icons";
+import { useStoredPlaces } from "../App.store";
 type Weather = {
   description: string;
 };
 export function Details({ route }: any) {
   const { location, name, navigation } = route.params;
-
-  const { isLoading, data = [] } = usePlaceWeatherForecast(
+  const units = useStoredPlaces((state) => state.units);
+  const { isLoading, data } = usePlaceWeatherForecast(
     location.lat,
     location.lon,
-    "metric"
+    units
   );
   return isLoading ? (
     <View>
       <ActivityIndicator size={20} color={"#000"} />
     </View>
   ) : (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          marginTop: 50,
-          marginLeft: 20,
-        }}
-      >
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Cities")}
-          style={{ height: 30, width: 30 }}
+    data && (
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            marginTop: 50,
+            marginLeft: 20,
+          }}
         >
-          <Ionicons name="arrow-back" size={30} />
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Cities")}
+            style={{ height: 30, width: 30 }}
+          >
+            <Ionicons name="arrow-back" size={30} />
+          </TouchableOpacity>
+        </View>
 
-      <View style={{ alignItems: "center" }}>
-        <Text style={{ fontSize: 40 }}>
-          {name.substring(0, name.indexOf(","))}
-        </Text>
-        <Text style={{ fontSize: 85, marginLeft: 15 }}>
-          {Math.round(data.current.temp)}º
-        </Text>
-        {data.current.weather.map((weather: Weather) => {
-          return <Text style={{ fontSize: 25 }}>{weather.description}</Text>;
-        })}
-      </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 40 }}>
+            {name.substring(0, name.indexOf(","))}
+          </Text>
+          <Text style={{ fontSize: 85, marginLeft: 15 }}>
+            {Math.round(data.current.temp)}º
+          </Text>
+          {data.current.weather.map((weather: Weather) => {
+            return <Text style={{ fontSize: 25 }}>{weather.description}</Text>;
+          })}
+        </View>
 
-      <View
-        style={{
-          flexGrow: 1,
-          flex: 1,
-          alignItems: "center",
-          paddingBottom: 10,
-        }}
-      >
-        <Text style={{ fontSize: 15 }}>Previsão para os próximos 5 dias</Text>
-        <FlatList
-          keyExtractor={(i) => i.dt}
-          data={data.daily.slice(0, 6)}
-          renderItem={({ item, index }: any) => (
-            <CardDetailsDays item={item} index={index} />
-          )}
-        />
+        <View style={styles.containerWeatherDays}>
+          <Text style={{ fontSize: 15 }}>Previsão para os próximos 5 dias</Text>
+          <FlatList
+            keyExtractor={(i) => String(i.dt)}
+            data={data.daily.slice(0, 6)}
+            renderItem={({ item, index }) => (
+              <CardDetailsDays item={item} index={index} />
+            )}
+          />
+        </View>
       </View>
-    </View>
+    )
   );
 }
 
@@ -78,5 +74,11 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+  },
+  containerWeatherDays: {
+    flexGrow: 1,
+    flex: 1,
+    alignItems: "center",
+    paddingBottom: 10,
   },
 });
