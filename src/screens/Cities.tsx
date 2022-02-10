0e371d851352d055da1f66266a1edec7c66d16eb
Nauã Fernandes <nauafernandesvv@gmail.com>
2022-02-10 13:27:48 -0300

    chore: move favorites to top

--- src/screens/Cities.tsx
+++ src/screens/Cities.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import {
   Text,
   View,
@@ -10,6 +10,7 @@ import { useStoredPlaces } from "../App.store";
 import { CardCityWeather } from "../components/CardCityWeather";
 import { statusBarHeight } from "../App.constants";
 import { FontAwesome } from "@expo/vector-icons";
+
 type Props = {
   navigation: {
     navigate: (to: string) => void;
@@ -21,16 +22,17 @@ export function Cities(props: Props) {
   const storedPlaces = useStoredPlaces((state) => state.places);
   const units = useStoredPlaces((state) => state.units);
   const changeUnits = useStoredPlaces((state) => state.changeUnits);
+  const favorites = useStoredPlaces((state) => state.favorites);
+
+  const cards = useMemo(() => {
+    return [
+      storedPlaces.filter((i) => favorites.includes(i.place_id)),
+      storedPlaces.filter((i) => !favorites.includes(i.place_id)),
+    ].flat();
+  }, [storedPlaces, favorites]);
   return (
     <View style={styles.root}>
-      <View
-        style={{
-          margin: 20,
-          alignContent: "center",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
+      <View style={styles.header}>
         <Text style={{ fontSize: 30 }}>Cidades</Text>
         <TouchableOpacity
           style={{
@@ -41,14 +43,7 @@ export function Cities(props: Props) {
           <FontAwesome name="search-plus" size={30} />
         </TouchableOpacity>
       </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "flex-end",
-          marginTop: 20,
-          marginRight: 20,
-        }}
-      >
+      <View style={styles.containerChangeMetric}>
         <TouchableOpacity
           style={{ marginRight: 10 }}
           onPress={() => changeUnits("metric")}
@@ -61,7 +56,7 @@ export function Cities(props: Props) {
       </View>
       {storedPlaces.length > 0 ? (
         <FlatList
-          data={storedPlaces}
+          data={cards}
           contentContainerStyle={{ alignItems: "center" }}
           keyExtractor={(i) => i.place_id}
           renderItem={({ item }) => (
@@ -73,14 +68,7 @@ export function Cities(props: Props) {
           )}
         />
       ) : (
-        <View
-          style={{
-            alignContent: "center",
-            justifyContent: "center",
-            alignItems: "center",
-            flex: 1,
-          }}
-        >
+        <View style={styles.containerEmptyList}>
           <Text style={{ textAlign: "center", padding: 20 }}>
             Nenhuma cidade adicionada, adicione usando o botão de pesquisa
           </Text>
@@ -94,5 +82,23 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     marginTop: statusBarHeight,
+  },
+  containerEmptyList: {
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  header: {
+    margin: 20,
+    alignContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  containerChangeMetric: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 20,
+    marginRight: 20,
   },
 });
